c98bbb00f3ae25e0a0e8b4291e002eacda3f0fb3
William O'Hanley <me@wohanley.com>
2022-08-04 13:04:51 -0700

    Support comments and compound args to literals

--- grammar.js
+++ grammar.js
@@ -2,31 +2,44 @@ module.exports = grammar({
   name: 'epilog',
 
   rules: {
-    code: $ => repeat(choice($.rule, $.literal)),
+    code: $ => repeat(choice($.comment, $.literal, $.rule)),
 
-    literal: $ => seq(optional('~'),
+    comment: $ => token(seq('%', /.*/)),
+
+    literal: $ => seq(optional($.op_negate),
                       field('predicate', $.constant),
                       optional(
                         seq(
                           '(',
-                          optional($.literal_args),
+                          field('args', optional($.literal_args)),
                           ')'))),
+
+    op_negate: $ => '~',
 
     literal_args: $ => seq($._argument, repeat(seq(',', $._argument))),
 
-    rule: $ => prec(400, seq(field('head', $.literal), ':-', $.rule_body)),
+    rule: $ => prec(400, seq(field('head', $.literal), ':-', field('body', $.rule_body))),
 
     rule_body: $ => seq($.literal, repeat(seq('&', $.literal))),
 
     _argument: $ => choice($._term, $.variable),
 
-    _term: $ => choice($.constant, $.compound_expr),
+    _term: $ => choice($.constant, $.compound),
 
-    compound_expr: $ => seq($.constant, '(', $._term, repeat(seq(',', $._term)), ')'),
+    compound: $ =>
+      seq(
+        field('constructor', $.constant),
+        '(',
+        field('args', $.compound_args),
+        ')'
+      ),
+
+    compound_args: $ => seq(choice($._term, $.variable),
+                            repeat(seq(',', choice($._term, $.variable)))),
 
     constant: $ => /[a-z\d_\.]+/,
 
-    variable: $ => prec(500, /[A-Z][a-z\d_\.]*/),
+    variable: $ => prec(500, /[A-Z][A-Za-z\d_\.]*/),
 
     // TODO escape quotes
     string: $ => seq('"', /[^"]+/, '"')
